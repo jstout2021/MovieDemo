63e12d550f3b14e0af39f033c631daa25d2011aa
Jason Stout <jsamui@icloud.com>
2021-06-21 13:47:05 -0500

    Initial Commit

---
+++ MovieDemo/MovieDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDemoApp.swift
+//  MovieDemo
+//
+//  Created by Jason Stout on 6/21/21.
+//
+
+import SwiftUI
+
+@main
+struct MovieDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
